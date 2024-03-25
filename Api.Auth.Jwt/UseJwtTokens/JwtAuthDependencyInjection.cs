6a228ce998848741437a17366444eb4d39a6b1e7
b.a.n <barmin.andrey.nnov@gmail.com>
2024-03-25 22:11:51 +0300

    Auth.JWT

---
+++ Api.Auth.Jwt/UseJwtTokens/JwtAuthDependencyInjection.cs
@@ -0,0 +1,44 @@
+﻿using Api.Auth.Jwt.UseJwtTokens.UserAccessor;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Api.Auth.Jwt.UseJwtTokens;
+
+public static class JwtAuthDependencyInjection
+{
+    public static void AddUserAccessor(this IServiceCollection services)
+    {
+        services.AddHttpContextAccessor();
+        services.AddScoped<IUser, UserAccessor.UserAccessor>();
+    }
+
+    public static void AddTestUserAccessor(this IServiceCollection services)
+    {
+        services.AddScoped<IUser, TestUserAccessor>();
+    }
+
+    public static void AddJwtBearer(this IServiceCollection services, IConfiguration configuration)
+    {
+        var JWTSettings = configuration.GetSection("JWTSettings");
+        var jwtSecret = JWTSettings.GetValue<string>("Secret")
+            ?? throw new Exception("jwtSecret not found ");
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+         .AddJwtBearer(options =>
+         {
+             options.TokenValidationParameters = new TokenValidationParameters
+             {
+                 ValidateAudience = false,
+                 ValidateIssuer = false,
+                 ValidateLifetime = true,
+                 ValidateIssuerSigningKey = true,
+                 IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret))
+             };
+         });
+    }
+
+
+}
---
+++ Api.Auth.Jwt/UseJwtTokens/JwtAuthFactory.cs
@@ -0,0 +1,8 @@
+﻿using Api.Auth.Jwt.UseJwtTokens.UserAccessor;
+
+namespace Api.Auth.Jwt.UseJwtTokens;
+
+public static class JwtAuthFactory
+{
+    public static IUser GetTestUser() => new TestUserAccessor();
+}
