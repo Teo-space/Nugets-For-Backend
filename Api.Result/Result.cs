fb9875cd5979fe3010069503fe618267f3e617da
b.a.n <barmin.andrey.nnov@gmail.com>
2024-03-29 05:52:56 +0300

    extends result

--- Api.Result/Result.cs
+++ Api.Result/Result.cs
@@ -1,12 +1,6 @@
-﻿public record Result<T>
+﻿public record Result<T> : Result
 {
     public required T Value { get; init; }
-    public required bool Success { get; init; }
-    public required string Type { get; init; }
-    public required string Detail { get; init; }
-    public required IReadOnlyCollection<FieldError> Errors { get; init; } = new List<FieldError>();
-
-
 
     public static implicit operator T(Result<T> Result) => Result.Value;
     public static implicit operator Result<T>(T o) => Results.Ok<T>(o);
