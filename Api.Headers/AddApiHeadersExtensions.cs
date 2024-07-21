a3a880f40a89c6474a81aa8b2de63ff35d230fef
b.a.n <barmin.andrey.nnov@gmail.com>
2024-07-21 20:06:55 +0300

    Api.Headers: Add

---
+++ Api.Headers/AddApiHeadersExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+
+namespace Api.Headers;
+
+public static class ApiHeadersExtensions
+{
+    /// <summary>
+    /// Адаптированный набор прокинутых заголовков
+    /// </summary>
+    /// <param name="app">Контекст приложения</param>
+    public static IApplicationBuilder UseIxForwardedHeaders(this IApplicationBuilder app)
+    {
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+        });
+        return app;
+    }
+}
