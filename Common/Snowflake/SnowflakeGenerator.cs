595787c2c4aff9331488aefacf46784201c65fde
b.a.n <barmin.andrey.nnov@gmail.com>
2024-08-02 21:20:44 +0300

    Add Snowflake

---
+++ Common/Snowflake/SnowflakeGenerator.cs
@@ -0,0 +1,33 @@
+﻿using IdGen;
+
+namespace System;
+
+public static class SnowflakeGenerator
+{
+    public static long New() => Generator.CreateId();
+
+
+    public static DateTime Epoch => new DateTime(2024, 8, 2, 17, 0, 0, DateTimeKind.Utc);
+
+    public const byte TimestampBits = 43;
+    public const byte GeneratorIdBits = 0;
+    public const byte SequenceBits = 20;
+    public const int GeneratorId = 0;
+
+    public readonly static IdStructure Structure = new IdStructure(TimestampBits, GeneratorIdBits, SequenceBits);
+    public readonly static IdGeneratorOptions GeneratorOptions = new IdGeneratorOptions(Structure, new DefaultTimeSource(Epoch));
+    public readonly static IdGenerator Generator = new IdGenerator(GeneratorId, GeneratorOptions);
+
+
+    public static string Info()
+    {
+        return $@"
+Max. generators       : {Structure.MaxGenerators}
+Id's/ms per generator : {Structure.MaxSequenceIds}
+Id's/ms total         : {Structure.MaxGenerators * Structure.MaxSequenceIds}
+Wraparound interval   : {Structure.WraparoundInterval(Generator.Options.TimeSource)}
+Wraparound date       : {Structure.WraparoundDate(Generator.Options.TimeSource.Epoch, Generator.Options.TimeSource).ToString("O")}
+";
+    }
+
+}
---
+++ Common/Snowflake/SnowflakeId.cs
@@ -0,0 +1,11 @@
+﻿namespace System;
+
+public record struct Snowflake(long Value)
+{
+    public static Snowflake New() => new Snowflake(SnowflakeGenerator.New());
+
+
+    public static implicit operator long(Snowflake snowflakeId) => snowflakeId.Value;
+    public static implicit operator string(Snowflake snowflakeId) => snowflakeId.Value.ToString();
+
+}
