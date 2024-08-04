7a4f8089b51d4672230af9fe1e8fbdbb0a8a4041
b.a.n <barmin.andrey.nnov@gmail.com>
2024-08-04 11:37:32 +0300

    upd

--- Common/Snowflake/SnowflakeId.cs
+++ Common/Snowflake/SnowflakeId.cs
@@ -4,6 +4,7 @@ public record struct Snowflake(long Value)
 {
     public static Snowflake New() => new Snowflake(SnowflakeGenerator.New());
 
+    public static implicit operator Snowflake(long Value) => new Snowflake(Value);
 
     public static implicit operator long(Snowflake snowflakeId) => snowflakeId.Value;
     public static implicit operator string(Snowflake snowflakeId) => snowflakeId.Value.ToString();
