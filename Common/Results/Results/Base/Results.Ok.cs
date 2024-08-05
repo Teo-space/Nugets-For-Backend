2ba969fd8ec63e1f3c4f97e69c54574a96594e51
b.a.n <barmin.andrey.nnov@gmail.com>
2024-08-05 21:15:27 +0300

    upd Result

--- Common/Results/Results/Base/Results.Ok.cs
+++ Common/Results/Results/Base/Results.Ok.cs
@@ -3,4 +3,7 @@
 	public static Result<T> Ok<T>(T Value) 
 		=> Results.Create<T>(Value, true, Statuses.Ok, default);
 
+	public static Result<T> Ok<T>(T Value, string detail) 
+		=> Results.Create<T>(Value, true, Statuses.Ok, detail);
+
 }
