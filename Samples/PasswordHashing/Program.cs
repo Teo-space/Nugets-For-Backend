5495b723189dcf752788dac9a0c77f7fce074d40
b.a.n <barmin.andrey.nnov@gmail.com>
2024-08-05 20:31:28 +0300

    Move projects to folders

---
+++ Samples/PasswordHashing/Program.cs
@@ -0,0 +1,16 @@
+﻿string password = "12345678";
+
+print("Start Hash");
+var hashedPassword = PasswordHasher.String.Hash(password);
+print("Hashed");
+
+print(hashedPassword.Hash);
+print(hashedPassword.Salt);
+
+print("Start Verify");
+var varify = PasswordHasher.String.Verify(password, hashedPassword.Hash, hashedPassword.Salt);
+print("Verified");
+
+print(varify);
+
+
