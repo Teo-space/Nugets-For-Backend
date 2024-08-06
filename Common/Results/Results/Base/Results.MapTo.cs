49919563297317cb2859ff05449df325b6549a13
b.a.n <barmin.andrey.nnov@gmail.com>
2024-08-06 06:55:42 +0300

    Add Results.MapTo

---
+++ Common/Results/Results/Base/Results.MapTo.cs
@@ -0,0 +1,8 @@
+﻿public partial class Results
+{
+    public static Result<TMapped> MapTo<TSource, TMapped>(Result<TSource> sourceResult)
+        => Results.Create<TMapped>(default, sourceResult.Success, sourceResult.Type, sourceResult.Detail);
+    public static Result<TMapped> MapTo<TSource, TMapped>(Result<TSource> sourceResult, string detail)
+        => Results.Create<TMapped>(default, sourceResult.Success, sourceResult.Type, detail);
+
+}
