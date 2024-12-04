128f9501d93d79138eb6ec8744e183b5a3f88d50
b.a.n <barmin.andrey.nnov@gmail.com>
2024-12-04 23:19:35 +0300

    Api.Logging update packages

--- Logging/Api.Logging/AddLoggingExtentions.cs
+++ Logging/Api.Logging/AddLoggingExtentions.cs
@@ -7,7 +7,11 @@ public static class AddLoggingExtentions
 {
     public static ILoggingBuilder AddSerilogLogging(this ILoggingBuilder loggingBuilder, IConfiguration configuration)
     {
-        var loggerConfiguration = new LoggerConfiguration().ReadFrom.Configuration(configuration);
+        var loggerConfiguration = new LoggerConfiguration()
+            .ReadFrom
+            .Configuration(configuration)
+            ;
+
         var logger = loggerConfiguration.CreateLogger();
 
         loggingBuilder.ClearProviders();
