ed0a402347807f29d7e73875ab580dbc1af28366
b.a.n <barmin.andrey.nnov@gmail.com>
2024-12-05 19:26:12 +0300

    Separate User

--- Api/Api.Auth.Jwt/JwtAuthDependencyInjection.cs
+++ Api/Api.Auth.Jwt/JwtAuthDependencyInjection.cs
@@ -1,5 +1,4 @@
-﻿using Api.Auth.Jwt.UseJwtTokens.UserAccessor;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -7,17 +6,6 @@ using System.Text;
 
 public static class JwtAuthDependencyInjection
 {
-    public static void AddUserAccessor(this IServiceCollection services)
-    {
-        services.AddHttpContextAccessor();
-        services.AddScoped<IUser, UserAccessor>();
-    }
-
-    public static void AddTestUserAccessor(this IServiceCollection services)
-    {
-        services.AddScoped<IUser, TestUserAccessor>();
-    }
-
     public static void AddJwtBearer(this IServiceCollection services, IConfiguration configuration)
     {
         var JWTSettings = configuration.GetSection("JWTSettings");
--- Api/Api.Auth.Jwt/UseJwtTokens/JwtAuthFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿using Api.Auth.Jwt.UseJwtTokens.UserAccessor;
-
-namespace Api.Auth.Jwt.UseJwtTokens;
-
-public static class JwtAuthFactory
-{
-    public static IUser GetTestUser() => new TestUserAccessor();
-}
