eaaf017f9b753a9b599af84954609dc0a20dba66
b.a.n <barmin.andrey.nnov@gmail.com>
2025-01-30 22:01:58 +0300

    add OpenTelemetry

---
+++ Api/Api.OTel/OTelDependencyInjection.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace Api.OTel;
+
+public static class OTelDependencyInjection
+{
+    public static IServiceCollection AddOTel(this IServiceCollection services, string serviceName, Uri endpoint = default)
+    {
+        services.AddOpenTelemetry()
+            .ConfigureResource(resource =>
+            {
+                resource.AddService(serviceName);
+                resource.AddEnvironmentVariableDetector();
+                //resource.AddAttributes();
+            })
+            .WithMetrics(metrics =>
+            {
+                metrics.AddRuntimeInstrumentation();
+                metrics.AddAspNetCoreInstrumentation();
+                metrics.AddHttpClientInstrumentation();
+
+                metrics.AddSqlClientInstrumentation();
+
+                if (endpoint != default)
+                {
+                    metrics.AddOtlpExporter(options =>
+                    {
+                        options.Endpoint = endpoint;
+                    });
+                }
+                else
+                {
+                    metrics.AddOtlpExporter();
+                }
+            })
+            .WithTracing(tracing =>
+            {
+                tracing.AddAspNetCoreInstrumentation();
+                tracing.AddHttpClientInstrumentation();
+
+                tracing.AddEntityFrameworkCoreInstrumentation();
+                tracing.AddSqlClientInstrumentation();
+#if DEBUG
+                tracing.AddConsoleExporter();
+#endif
+                if (endpoint != default)
+                {
+                    tracing.AddOtlpExporter(options =>
+                    {
+                        options.Endpoint = endpoint;
+                    });
+                }
+                else
+                {
+                    tracing.AddOtlpExporter();
+                }
+            })
+            ;
+
+
+
+        return services;
+    }
+
+    public static ILoggingBuilder AddOTel(this ILoggingBuilder loggingBuilder, Uri endpoint = default)
+    {
+        loggingBuilder.AddOpenTelemetry(logging =>
+        {
+            logging.IncludeFormattedMessage = true;
+            logging.IncludeScopes = true;
+
+            if (endpoint != default)
+            {
+                logging.AddOtlpExporter(options =>
+                {
+                    options.Endpoint = endpoint;
+                });
+            }
+            else
+            {
+                logging.AddOtlpExporter();
+            }
+        });
+
+        return loggingBuilder;
+    }
+}
--- Samples/Api.Tests/Program.cs
+++ Samples/Api.Tests/Program.cs
@@ -1,4 +1,5 @@
 using Api.Calls.LogTo.SqlServer.ColumnStore;
+using Api.OTel;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,9 @@ builder.Services.AddApiCallsLogger(options =>
     options.Interval = 5;
     options.BatchSize = 1000;
 });
+//OTelDependencyInjection
+builder.Services.AddOTel("Test");
+builder.Logging.AddOTel();
 
 var app = builder.Build();
 
